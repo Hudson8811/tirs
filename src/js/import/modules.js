d2b48e9a85e6e110c25465d48bdfe7918fc9b5bf
kihhmaster <kihhmaster@yandex.ru>
2023-09-12 18:50:30 +0300

    закончил

---
+++ src/js/import/modules.js
@@ -0,0 +1,1 @@
+@@include("../../blocks/modules/product-card-slider/product-card-slider.js")
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+//libs
+@@include("./libs/jquery-3.7.0.min.js")
+
+//my files
+@@include("./import/main.js")
+@@include("./libs/swiper-bundle.min.js")
+@@include("./libs/select2.min.js") 
+
+@@include("./import/components.js")
+@@include("./import/modules.js")
